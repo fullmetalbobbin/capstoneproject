781e376370e34d67c1a7e8dd0bba599ee938d4f3
fullmetalbobbin <akdreesen@ksu.edu>
2022-12-10 23:03:37 -0600

    server.js

---
+++ project/server.js
@@ -0,0 +1,31 @@
+/******************************************** 
+ *  server.js                               *
+ *                                          *
+ *  Capstone Project:  "Context Textiles"   *
+ *  Author: Amanda Dreesen                  *
+ *          Kansas State University         *
+ *          College of Engineering          *
+ *          Computer Science                *
+ ********************************************/
+
+// IMPORT REQUIRED RESOURCES
+require('./src/templates');
+require('./src/database');
+
+
+const http = require('http');
+const app = require('./src/app');
+
+const port = 8082;
+
+// Console log message for debugging purposes
+app.listen(port, () => {
+    console.log(`Starting port: ${port}`);
+});
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'Is this thing on?'
+    });
+});
+
